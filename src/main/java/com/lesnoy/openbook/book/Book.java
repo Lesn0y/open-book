1a93deb8349f56a40cef5dfe45827c8c066d7fd6
Lesn0y <kilriljob@gmail.com>
2023-01-24 15:56:40 +0300

    Refactor redundant import

--- src/main/java/com/lesnoy/openbook/book/Book.java
+++ src/main/java/com/lesnoy/openbook/book/Book.java
@@ -1,7 +1,6 @@
 package com.lesnoy.openbook.book;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.*;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
