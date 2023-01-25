3350f34bb31e5f983b9548235274b2753e50d45a
Lesn0y <kilriljob@gmail.com>
2023-01-25 18:14:38 +0300

    Refactored the book and all its connections

--- src/main/java/com/lesnoy/openbook/book/Book.java
+++ src/main/java/com/lesnoy/openbook/book/Book.java
@@ -1,19 +1,45 @@
 package com.lesnoy.openbook.book;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.lesnoy.openbook.author.Author;
+import com.lesnoy.openbook.feedback.Feedback;
+import com.lesnoy.openbook.history.History;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@JsonSerialize(using = BookSerializer.class)
+@Entity
+@Table(name = "book", schema = "open_book")
 public class Book {
-    private String id;
-    private String title;
-    private String authors;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "thumbnail_url")
     private String thumbnailUrl;
+    @Column(name = "book_name")
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+    @Column(name = "pagecount")
+    private int pageCount;
+    @Column(name = "subtitle")
+    private String subtitle;
+    @Column(name = "description")
+    private String description;
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
+    private List<Feedback> feedbacks;
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
+    private List<History> history;
 }
 
 
--- src/main/java/com/lesnoy/openbook/book/BookRepository.java
+++ src/main/java/com/lesnoy/openbook/book/BookRepository.java
@@ -1,2 +1,6 @@
-package com.lesnoy.openbook.book;public interface BookRepository {
+package com.lesnoy.openbook.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
 }
