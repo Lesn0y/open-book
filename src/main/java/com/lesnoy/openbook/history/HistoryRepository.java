9568b58fc3ce195cac72a9045b143bffabf27367
Lesn0y <kilriljob@gmail.com>
2023-02-10 19:58:38 +0300

    Restructured the architectur to add angular(step 2)

--- src/main/java/com/lesnoy/openbook/history/HistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lesnoy.openbook.history;
-
-import com.lesnoy.openbook.book.UserBookPKId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HistoryRepository extends JpaRepository<History, UserBookPKId> {
-}
--- src/main/java/com/lesnoy/openbook/user/Role.java
+++
@@ -1,6 +0,0 @@
-package com.lesnoy.openbook.user;
-
-public enum Role {
-    USER,
-    ADMIN
-}
