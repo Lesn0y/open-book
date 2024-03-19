543dd791e3cdadd5f386ac201adc01658034ca5e
Lesn0y <kilriljob@gmail.com>
2024-03-19 11:42:20 +0300

    Add controller

---
+++ InventoryService/src/main/java/org/lesnoy/inventoryservice/controller/BookController.java
@@ -0,0 +1,53 @@
+package org.lesnoy.inventoryservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.lesnoy.inventoryservice.dto.BookRequest;
+import org.lesnoy.inventoryservice.exceptions.BookNotFoundException;
+import org.lesnoy.inventoryservice.model.Book;
+import org.lesnoy.inventoryservice.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/book")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService service;
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks(@RequestParam(value = "in_stock", required = false) Boolean inStock) {
+        if (inStock != null) {
+            return ResponseEntity.ok((service.getAllBooksInStock(inStock)));
+        }
+        return ResponseEntity.ok(service.getAllBooks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable("id") Long id) throws BookNotFoundException {
+        return ResponseEntity.ok(service.getBookById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> saveBook(@RequestBody BookRequest bookDTO) {
+        return new ResponseEntity<>(service.saveBook(bookDTO), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBookById(@PathVariable("id") Long id,
+                                               @RequestBody(required = false) BookRequest bookDTO) throws BookNotFoundException {
+        if (bookDTO == null) {
+            return ResponseEntity.ok(service.changeStateInStock(id));
+        }
+        return ResponseEntity.ok(service.updateBook(id, bookDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteBookById(@PathVariable("id") Long id) throws BookNotFoundException {
+        service.deleteBookById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
